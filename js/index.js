c969b4c7d703badebdab5c035d9dfabe46bb6e88
Azizbek Begmurodovich <begmurodovich.fullstack.dev@gmail.com>
2026-03-17 16:23:25 +0500

    version 1.30

--- js/index.js
+++ js/index.js
@@ -117,3 +117,27 @@ window.addEventListener('click', function(e) {
     closeModal();
   }
 });
+
+
+
+// 
+// 
+// 
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
